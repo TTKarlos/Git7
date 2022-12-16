7b637f8e368b68cf1f6c066c5639efff49d4525a
Karlos <carlossayubu@gmail.com>
2022-12-16 01:14:29 +0100

    [MOD] Cambiado numero 20 por 50

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        imprimir20Numeros(20);
+        imprimir20Numeros(50);
     }
     public static void imprimir20Numeros (int numeros) {
         for (int i = 1; i < numeros + 1; i++) {
